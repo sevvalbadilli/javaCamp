03b52dbb27edd7ae0829fd0dfb720fbddaa4edba
sevvalbadilli <sevvalbadilli0@gmail.com>
2022-11-14 15:31:51 +0300

    deneme

---
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/HibernateProductDao.java
@@ -0,0 +1,12 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Product;
+
+public class HibernateProductDao implements ProductDao{
+	
+	public void add(Product product  ) {
+		// sadece ve sadece db erişim kodları yazılır SQL
+		System.out.println("hibernate ile veri tabanına eklendi");
+	}
+
+}
---
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/ProductDao.java
@@ -0,0 +1,12 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Product;
+
+public interface ProductDao {
+	
+	void add(Product product);
+	
+	
+	
+
+}
